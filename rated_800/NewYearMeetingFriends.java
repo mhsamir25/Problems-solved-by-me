bc3c35bb6a0b01f901c7d93820fd1a4b0904c9cf
Mahmudul Hossain Samir <mahmudul1@iut-dhaka.edu>
2025-06-28 10:49:58 +0600

    making some changes

---
+++ rated_800/NewYearMeetingFriends.java
@@ -0,0 +1,18 @@
+package rated_800;
+import java.util.*;
+public class NewYearMeetingFriends {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        int median = arr[1];
+
+        int result = Math.abs(median-arr[0])+Math.abs(median-arr[2]);
+        System.out.println(result);
+    }
+}
+//https://codeforces.com/problemset/problem/723/A
