102f347632daed6bb236b2b9f70bd050b8251bcc
Mahmudul Hossain Samir <mahmudul1@iut-dhaka.edu>
2025-07-17 17:09:21 +0600

    making some changes

---
+++ rated_800/WordOnThePaper.java
@@ -0,0 +1,37 @@
+package rated_800;
+import java.util.*;
+public class WordOnThePaper {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        ArrayList<String> a = new ArrayList<>();
+        while(t-- >0){
+            String[] grid = new String[8];
+            for(int i=0;i<8;i++){
+                grid[i]=sc.nextLine();
+            }
+            String word = "";
+           
+            for(int i=0;i<8;i++){
+                StringBuilder rowWord = new StringBuilder();
+                for(int j = 0;j<8;j++){
+                char c = grid[i].charAt(j);
+                if(c>='a' && c<= 'z'){
+                    rowWord.append(c);
+                }
+
+                if(rowWord.length()>0){
+                    word = rowWord.toString();
+                }
+                a.add(word);
+            }
+
+            }
+        }
+
+        for(String result: a){
+            System.out.println(result);
+        }
+    }
+}
---
+++ rated_900/OddGrasshopper.java
@@ -0,0 +1,39 @@
+package rated_900;
+import java.util.*;
+
+public class OddGrasshopper {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        while (t-- > 0) {
+            long x = sc.nextLong();
+            long n = sc.nextLong();
+            sb.append(getFinalPosition(x, n)).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    static long getFinalPosition(long x, long n) {
+        long q = n / 4;
+        int r = (int)(n % 4);
+        
+        long result = x;
+        if (x % 2 == 0) {
+            result += 2 * q;
+            if (r == 1) result -= 1;
+            else if (r == 2) result += 1;
+            else if (r == 3) result -= 2;
+        } else {
+            result -= 2 * q;
+            if (r == 1) result += 1;
+            else if (r == 2) result -= 1;
+            else if (r == 3) result += 2;
+        }
+        return result;
+        
+    }
+    
+}
